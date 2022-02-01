11805a7d79892f5049efa0e0a9782f33f059b97d
gardianos <gardianii@hotmail.com>
2022-02-01 15:52:36 +0100

    configuring microservice and modify the port to export

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
+//
+// Throws an error if the PORT environment variable is missing.
+//
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
 
-const port = 4000;
+//
+// Extracts the PORT environment variable.
+//
+const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello Microservice app");
+app.get("/video", (req, res) => {
+  const path = "./videos/my-video.mp4";
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.log("An error occurred");
+      res.sendStatus(500);
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(path).pipe(res);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`App started on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
 });
